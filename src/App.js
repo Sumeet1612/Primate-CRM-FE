903acf01fb1acb30a4988ecc5a4eaec3f72b4c89
Shubham Prasad <shubhampd1990@gmail.com>
2023-07-16 22:57:47 +0530

    Added broker feature to EditLoad

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NewLoad from './components/NewLoad';
 import EditLoad from './components/EditLoad';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Welcome to CRM</h1>
     <BrowserRouter>
     <Routes>
+      <Route exact path="/" element={<Home/>}/>
       <Route path = "New" element = {<NewLoad/>}/>
       <Route path = "Edit" element = {<EditLoad/>}/>
     </Routes>
