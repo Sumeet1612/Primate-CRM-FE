fec6f7e6f3b7f068f3985af3a613a03cedc47f9c
Sumeet <sumeet.singh1612@gmail.com>
2023-07-21 11:22:15 -0400

    Added new pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import NewLoad from './components/NewLoad';
-import EditLoad from './components/EditLoad';
-import Home from './components/Home';
-import Shippers from './components/Shippers'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import NewLoad from "./components/NewLoad";
+import EditLoad from "./components/EditLoad";
+import Home from "./components/Home";
+import Shippers from "./components/Shippers";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to CRM</h1>
-    <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route path = "New" element = {<NewLoad/>}/>
-      <Route path = "Edit" element = {<EditLoad/>}/>
-      <Route path = "Shippers" element = {<Shippers/>}/>
-    </Routes>
-    </BrowserRouter>
+      <Navigation />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="New" element={<NewLoad />} />
+          <Route path="Edit" element={<EditLoad />} />
+          <Route path="Shippers" element={<Shippers />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/SidebarData.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const SidebarData = [
+  {
+    title: "Dashboard",
+    path: "/",
+
+    cName: "nav-text",
+  },
+  {
+    title: "Loads",
+    path: "/new",
+
+    cName: "nav-text",
+  },
+  {
+    title: "Shipper",
+    path: "/shipper",
+
+    cName: "nav-text",
+  },
+  {
+    title: "Profile",
+    path: "/profile",
+
+    cName: "nav-text",
+  },
+  {
+    title: "Invoice",
+    path: "/invoice",
+
+    cName: "nav-text",
+  },
+];
