33a709fca69848b3e8fac356775791b27b533c4a
Sumeet <sumeet.singh1612@gmail.com>
2023-07-21 11:33:28 -0400

    reftified again

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to CRM</h1>
-      <Navigation />
+      {/* <Navigation /> */}
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
