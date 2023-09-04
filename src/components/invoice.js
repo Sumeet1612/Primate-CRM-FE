5ddd94e3620c091f2be9313226ab338c45bc30c7
Sumeet <sumeet.singh1612@gmail.com>
2023-09-04 08:22:03 -0400

    UI Fixes

--- src/components/invoice.js
+++ src/components/invoice.js
@@ -1,7 +1,7 @@
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
-import Box from "@mui/material/Box";
+// import TextField from "@mui/material/TextField";
+// import Button from "@mui/material/Button";
+// import AddIcon from "@mui/icons-material/Add";
+// import Box from "@mui/material/Box";
 
 function Invoice() {
   return (
